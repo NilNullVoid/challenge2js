d40a89feed5584f1bc7de52aa6ce68daa373d4e2
NilNullVoid <103994960@swin.edu.au>
2022-06-15 15:21:15 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+search = (id) => document.getElementById(id);
+let timeoutids = [];
+search('roll').onclick = () => {
+  timeoutids.forEach((id) => clearTimeout(id));
+  if (search('times').value > 20) {
+    search('list').innerText = "can't go above 20!";
+    return;
+  }
+  for (let i = 0; i < search('times').value; ) {
+    timeoutids.push(
+      setTimeout(() => {
+        search('list').innerText += ` ${Math.floor(Math.random() * 6 + 1)}`;
+        average();
+        total();
+      }, i++ * 1000)
+    );
+  }
+};
+
+search('average').onclick = average;
+
+search('total').onclick = total;
+
+search('clear').onclick = () => {
+  timeoutids.forEach((id) => clearTimeout(id));
+  search('to').innerText = 0;
+  search('av').innerText = 0;
+  search('list').innerText = '';
+};
+
+average = () => {
+  search('av').innerText =
+    search('list')
+      .innerText.split(' ')
+      .reduce((a, b) => Number(a) + Number(b)) /
+    search('list').innerText.split(' ').length;
+};
+total = () => {
+  search('to').innerText = search('list')
+    .innerText.split(' ')
+    .reduce((a, b) => Number(a) + Number(b));
+};
